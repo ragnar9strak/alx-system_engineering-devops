1a1f5f92485cb01619ff130d3949326a5b6e33be
Monssef baakka <117485130+ragnar9strak@users.noreply.github.com>
2023-12-03 22:19:03 +0100

    Updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# 1-install_a_package.pp
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# install puppet-lint -v 2.5.0
+
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
